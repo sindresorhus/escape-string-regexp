6c325245db6668e9b21ce190edaa185d11938e03
Sindre Sorhus <sindresorhus@gmail.com>
2014-06-24 16:58:00 +0200

    init

---
+++ test.js
@@ -0,0 +1,7 @@
+'use strict';
+var assert = require('assert');
+var escapeStringRegexp = require('./');
+
+it('should escape RegExp special characters', function () {
+	assert.strictEqual(escapeStringRegexp('\\ ^ $ * + ? . ( ) | { } [ ]'), '\\\\ \\^ \\$ \\* \\+ \\? \\. \\( \\) \\| \\{ \\} \\[ \\]');
+});
