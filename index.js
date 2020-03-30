1e9a285078d13319461426b309d2e9b1a6dcc16f
Charmander <~@charmander.me>
2020-03-29 21:16:44 -0700

    Make replacement styles consistent and explain why they’re separate

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const matchOperatorsRegex = /[|\\{}()[\]^$+*?.]/g;
-
 module.exports = string => {
 	if (typeof string !== 'string') {
 		throw new TypeError('Expected a string');
 	}
 
+	// Escape characters with special meaning either inside or outside character sets.
+	// Use a simple backslash escape when it’s always valid, and a \unnnn escape when the simpler form would be disallowed by Unicode patterns’ stricter grammar.
 	return string
-		.replace(matchOperatorsRegex, '\\$&')
+		.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
 		.replace(/-/g, '\\u002d');
 };
